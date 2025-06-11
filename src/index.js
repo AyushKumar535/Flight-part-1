9aa9d6a42de12203887a74f6785509addcf5983a
AyushKumar535 <ayushrana03dec2003@gmail.com>
2025-06-11 17:08:33 +0530

    Added basic node express functionalities

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const {ServerConfig} = require('./config');
+const apiRoutes = require('./routes'); 
+
+// const AboutController = require('./controllers/about-controller');
+// const HomeController = require('./controllers/home-controller');
+
+// const {AboutController , HomeController} = require('./controllers')
+
+const app = express();
+
+app.use('/api',apiRoutes);  
+// console.log(process.env)
+app.listen(ServerConfig.PORT, () => {
+    console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+});
